644bfbc9d92a0c8a8b370c16e13eda479b9dcb1f
botfigther02 <abuhurayraniloy02@gmail.com>
2024-11-07 17:07:56 +0600

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-// app.js
 const PORT = process.env.PORT || 5000;
 const express = require('express');
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
-=======
-const PORT = process.env.PORT || 5000;
-const express = require('express');
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
->>>>>>> 34469114777496523f99062a0242ac2d358193ac
