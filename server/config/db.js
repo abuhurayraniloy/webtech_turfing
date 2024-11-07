e7f2364f48d2b4006b000e035449b986dca3d5b6
Abu Hurayra Niloy <105109331+abuhurayraniloy@users.noreply.github.com>
2024-11-07 16:51:28 +0600

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -11,17 +10,3 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool.promise();
-=======
-const mysql = require('mysql2');
-const dotenv = require('dotenv');
-dotenv.config();
-
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'chonchola',
-  database: 'turfbooker',
-});
-
-module.exports = pool.promise();
->>>>>>> 34469114777496523f99062a0242ac2d358193ac
